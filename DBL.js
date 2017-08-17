58fd79ed8f51c9c7ee07bcbe8cd7af95355b7348
torekndsn <storetoreknudsen@gmail.com>
2017-08-17 21:26:35 +0200

    delete function 50% done

--- DBL.js
+++ DBL.js
@@ -13,9 +13,13 @@ function DBL(){
 	var lastKeyHit = 0;
 	var lastWordLength = 0;
 	var previousKey;
-	var lastWord = "";
-	var typing; 
 
+	var lastWord = "";
+	var lastWordMeta = "";
+	var fontSize; 
+
+
+	var deletedWordsCount = 0; 
 	var spacing;
 
 
@@ -25,7 +29,6 @@ function DBL(){
 	 	currentMillis = event.timeStamp;
 		// get input text and get every word into array. 
 		var inputText = this.value;
-		
 		words = inputText.split(" ");
 
 		//reset time if nothing has been typed
@@ -39,6 +42,12 @@ function DBL(){
 
 		}
 
+
+
+
+
+
+
 		// S P A C I N G // - if break between words, get time interval to spacing
 		if(previousKey == " " || event.key == " "){
 			spacing = word_spacing(timeData);
@@ -47,23 +56,50 @@ function DBL(){
 			}
 		}
 		else spacing = 0; 
+
+		// D E L E T I N G // - if words are delted, it chould affect font-size of next word
+		if( words.length < lastWordLength) deletedWordsCount++; 
+		//console.log("deletedWordsCount: " + deletedWordsCount);
+
+
+
+		//get last word for meta information
+		lastWordMeta = words[words.length-1];
 			
 		// E N D   R E C O R D - if space is hit, a word is done. 
 		if(event.key == " " && words.length > 0){
+
+			fontSize = word_size(deletedWordsCount);
+		//	console.log("Calculated font size: " + fontSize);
+
 			lastWord = words[words.length-1];
-			values.push({word: lastWord, spacing: spacing });
+			values.push({word: lastWord, spacing: spacing, size: fontSize });
+
+			//reset 
+			deletedWordsCount = 0;
 		}
-		console.log("last spacing: " + values[values.length-1].spacing);
-		console.log("last word: " + values[values.length-1].word);
 	
-		 // if words have been deleted, remove from arrat. 
+		// D E L E T E    W O R D S   F R O M    A R R A Y - if words have been deleted, remove from array. 
 
 		while(values.length > words.length) values.splice(values.length-1, 1);
 		var currentWord = values[values.length-1].word;
 		if(lastWord != currentWord) values[values.length-1].word = lastWord; 
+
+		console.log("words: " + words);
+		console.log("last word: " + lastWordMeta);
+		console.log("previous key: " + previousKey);
+		console.log("last word length: " + lastWordLength);
+
+		//Recover fontSize for deleted words
+
+		 values[values.length-1].size = fontSize;
+		
 	 
-		//console.log("last word: " + words[words.length-1]);
-		//console.log("last value: " + values[values.length-1].word);
+		
+
+
+
+
 
 
 
@@ -71,17 +107,26 @@ function DBL(){
 
 		$("#outputText").empty();
  			jQuery.each(values, function(i, v) { 	
+
 			 	var w = values[i].word;
 			 	var spacing_ = values[i].spacing;
-
+			 	var size_ = values[i].size;
+	
 			 	var newStyle = "<span " + 
 				"style=\"padding-right:" + spacing_ +"px" + 
+				";font-size:" + size_ + "px" +
 				";\">";
 			//	console.log(newStyle + w + " " + "</span>");	
 				$("#outputText").append( $(newStyle + w + " " + "</span>"));
 			});
 
-
+ 		/* 	var newStyle = "<span " + 
+				"style=\"color:" + newCol +
+				";word-spacing:" + spacing_ +"px" + 
+				";font-size:" + size + "px" +
+				";letter-spacing:" + tracking_ + "px" +
+				";\">";
+		*/ 
 
  
 
