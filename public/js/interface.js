dd41849dbe32348fe74803701e3f492c186f6705
torekndsn <storetoreknudsen@gmail.com>
2018-04-12 13:26:51 +0200

    done

--- public/js/interface.js
+++ public/js/interface.js
@@ -64,7 +64,6 @@ function interface(){
 	     					}
 				  		})
 				  	}
-				
 				 });
 
 				 var answer2 = "empty";
@@ -72,9 +71,6 @@ function interface(){
 				  	if(event.key == 'Enter'){
 				  		answer2 = $("#input_2").val(); 
 				  		console.log("the answer: " + answer2);
-				  		if(wordInString(answer2, 'yes') || wordInString(answer2, 'YES')){
-				  			console.log("yes was a part");
-
 				  			$(".section2").fadeOut("normal", function() {
     				    		$(this).remove();
     						});
@@ -89,9 +85,7 @@ function interface(){
 	     					onComplete: (self) => {
 	     						 $( "#input_3" ).focus();
 	     					}
-				  		})
-
-				  		}
+				  		})		
 					}
 				}); 
 
@@ -132,217 +126,56 @@ function interface(){
 				 $("#input_4").keydown(function ( event) {
 					if(event.key == 'Enter') {
 						answer4 = $("#input_4").val(); 
-						console.log(answer4);
+						//console.log();
+
 
 						if(wordInString(answer4, 'go') || wordInString(answer4, 'GO'))
 				  		{
 				  			console.log("i'll go");
-				  			$("#screen1").fadeOut("normal", function() {
+				  			$(".section4").fadeOut("normal", function() {
     				    		$(this).remove();
     						});
 
     					var question = new Typed("#question", {
 
-    						strings: ["Question:</br></br>^1000How would you describe your relationship to your mom?"],
+    						strings: ["Question:</br>^1000"+questionArr[2]],
 							startDelay: 1000,
 							typeSpeed: slowType,
 							showCursor: false,
 							onComplete: (self) =>  {
 								$('#DBL-box').fadeIn('normal');
+								$("#input-section").fadeIn('normal');
 								$('body').css("overflow", "visible");
-								//	$('#inputText').focus();
+								$('#inputText').focus();
 							}
     					})	
-
 				  		}
 					}
-
 				 });
 
-				
-				
-			
- 
+				 $("#render").on('click', function(e) {
+					e.preventDefault(); 
+					$("#output-section").fadeIn('normal');
+				 }); 
 
-
-
-
-
-
-
-			 /* 
-			
-			 	 $( "#fname" ).keydown(function( event ) {
-			 	console.log("Hehllo");
-			 	if(event.key == 'Enter'){
-			 		if(wordInString($( "input:eq(0)" ).val(), 'yes') || wordInString($( "input:eq(0)" ).val(), 'YES'))
-				   {
-				  	$("#intro").fadeOut("normal", function() {
-    				    $(this).remove();
-    				});
-				    new Typed("#decisionintro", decisionintro);
-				    $('#div1').scrollTop($('#div1')[0].scrollHeight);
-				    event.preventDefault();
-				  }
-				 else if (wordInString($( "input:eq(0)" ).val(), 'no') || wordInString($( "input:eq(0)" ).val(), 'NO')){
-				 	// nothing
-				 }
-				 else {
-				 	event.preventDefault();
-				 }
-				}
-			});
-
-
-
-
-
-			 var screen2_2 = {
-			 	strings: ["Welcome, and thank you for starting the Digital Body Language.<br><br>^700What's about to happen is a performance. A performance from you.<br><br>^700It's going to be an embodiement of you action. An action you perform everyday, but an action which result dosen't reflect its process.<br><br>^700uhuh ... yih! i saw beyonces tizzles and my pizzle went crizzle sit amizzle, get down get down adipiscing elizzle. Nullizzle boofron velizzle, mofo go to hizzle, suscipizzle bizzle, gravida vizzle, arcu. Pellentesque go to hizzle tortizzle. Sizzle erizzle. Pizzle izzle dolor dapibus shiz tempizzle fo shizzle. Maurizzle pellentesque fo et turpizzle. Uhuh ..."],
-				startDelay: 1000,
-				typeSpeed: slowType,
-				showCursor: false,
-				onComplete: (self) =>  {
-
-					new Typed("#introend", introend);
-					$('#div1').scrollTop($('#div1')[0].scrollHeight);
-				}
-			 }
-
-			 var introend = {
-			 	strings: ["///////////////////////////////////////////////////////////////////////////"],
-				startDelay: 500,
-				typeSpeed: fastType,
-				showCursor: false,
-				onComplete: (self) =>  {
-					new Typed("#continued", continued);
-					$('#div1').scrollTop($('#div1')[0].scrollHeight);
-				}
-			 }
-
-
-
-			
-			 $( "#fname5" ).keydown(function( event ) {
-			 	console.log("Hehllo");
-			 	if(event.key == 'Enter'){
-			 		if(wordInString($( "input:eq(0)" ).val(), 'yes') || wordInString($( "input:eq(0)" ).val(), 'YES'))
-				   {
-				  	$("#intro").fadeOut("normal", function() {
-    				    $(this).remove();
-    				});
-				    new Typed("#decisionintro", decisionintro);
-				    $('#div1').scrollTop($('#div1')[0].scrollHeight);
-				    event.preventDefault();
-				  }
-				 else if (wordInString($( "input:eq(0)" ).val(), 'no') || wordInString($( "input:eq(0)" ).val(), 'NO')){
-				 	// nothing
-				 }
-				 else {
-				 	event.preventDefault();
-				 }
-				}
-			});
-
-
-
-			 // D I S I C I O N 
-
-			  var decisionintro = {
-			 	strings: ["///DECISION</br> ^700///////////////////////////////////////////////////////////////////////////"],
-				startDelay: 1000,
-				typeSpeed: fastType,
-				showCursor: false,
-				onComplete: (self) => {
-				 new Typed("#decisiontext", decisiontext);
-				 $('#div1').scrollTop($('#div1')[0].scrollHeight);
-				}
-			 }
-
-			  var decisiontext = {
-			 	strings: ["Your are going to do a peformance. And I'm going to ask you a question...</br></br>^700You answering the question with an answer from your heart, is going to be the performance</br></br>^700Are you ready to receive your question, and start the performance?"],
-				startDelay: 1000,
-				typeSpeed: slowType,
-				showCursor: false,
-				onComplete: (self) => {
-				 new Typed("#decisionend", decisionend);
-				 $('#div1').scrollTop($('#div1')[0].scrollHeight);
-				}
-			 }
-
-			  var decisionend = {
-			 	strings: ["///////////////////////////////////////////////////////////////////////////"],
-				startDelay: 500,
-				typeSpeed: fastType,
-				showCursor: false,
-				onComplete: (self) => {
-				 new Typed("#getquestion", getquestion);
-				 $('#div1').scrollTop($('#div1')[0].scrollHeight);
-				}
-			 }
-
-			 var getquestion = {
-				strings: ["If you are ready, type <b>'YES'</b> to get your question, or <b>'NO'</b> to stop."],
-				startDelay: 1000,
-				typeSpeed: slowType,
-				showCursor: false,
-				onComplete: (self) =>  {
-					$( "#fname2" ).focus();
-					$('#div1').scrollTop($('#div1')[0].scrollHeight);
-				}
-			}
-
-			$( '#fname2').keydown(function( event ){
-				
-				if(event.key == 'Enter'){
-					//event.preventDefault();
-					console.log("im typing enter...")
-					console.log( $( "#fname2" ).val());
-
-
-					if(wordInString($( "input:eq(0)" ).val(), 'yes') || wordInString($( "input:eq(0)" ).val(), 'YES'))
-					{
-						console.log("yes was typed");
-
-						$("#decision").fadeOut("normal", function() {
-					    	$(this).remove();
-					    }); 
-					    
-
-						new Typed("#question", question);
-						$('#div1').scrollTop($('#div1')[0].scrollHeight);
-						event.preventDefault();
-							 
-						}
-
-						else if(wordInString($( "input:eq(0)" ).val(), 'no') || wordInString($( "input:eq(0)" ).val(), 'NO')) 
-						{
-							//noting
-						}
-						else {
-							event.preventDefault();
-						}			
-				}
-			});
-
-				 // Q U E S T I O N
-		    
-			 var question = {
-				strings: ["Question:</br></br>^1000How would you describe your relationship to your mom?"],
-				startDelay: 1000,
-				typeSpeed: slowType,
-				showCursor: false,
-				onComplete: (self) =>  {
-					$('.content').fadeIn('normal');
-					$('body').css("overflow", "visible");
-				//	$('#inputText').focus();
-				}
-			}
-
-*/ 
 			// F U N C T I O N   B A N K
+			var questionArr = [
+			"How would you describe your relationship with your mother?",
+			"What is your earliest memory?", 
+			"Why is your best friend your best friend?",
+			"What is the hardest thing that has ever happened to you?",
+			"Can you describe your first kiss?",
+			"Which parent are you closer to and why?",
+			"If you could take back one thing you have done in life, what would it be?",
+			"What's the last thing that made you cry?",
+			"What's the stupidest thing you have ever done?",
+			"Have you ever broken somebody's heart? How?",
+			"What do you wish others would understand better about you?",
+			"What makes you feel like home?",
+			"If you could change anything about the way you were raised, what would you change?"
+			];
+
 			function wordInString(s, word){
  				 return new RegExp( '\\b' + word + '\\b', 'i').test(s);
 			}
-			
 		}
